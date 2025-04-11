f94c89344b9f21d7d93aa0a8e08a2c4b009bc056
Raphael Merx <raphael.merx@gmail.com>
2025-04-11 11:23:26 +0800

    Font: switch to Manrope

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import "@/styles/globals.css";
-import { Inter } from "@next/font/google";
+import { Manrope } from "@next/font/google";
 import type { AppProps } from "next/app";
 
-const inter = Inter({ subsets: ["latin"] });
+const manrope = Manrope({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps<{}>) {
   return (
-    <main className={inter.className}>
+    <main className={manrope.className}>
       <Component {...pageProps} />
     </main>
   );
