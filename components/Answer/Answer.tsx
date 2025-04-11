6785a488cfbe560cbfe8db9a06c5e9716c4baee2
Raphael Merx <raphael.merx@gmail.com>
2025-04-11 17:28:05 +0800

    Answer: fix appearing only after all text delivered

--- components/Answer/Answer.tsx
+++ components/Answer/Answer.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import styles from "./answer.module.css";
 import Markdown from "react-markdown";
 
 interface AnswerProps {
@@ -8,7 +7,7 @@ interface AnswerProps {
 
 export const Answer: React.FC<AnswerProps> = ({ text }) => {
   return (
-    <div className={`${styles.answer} ${text ? 'bg-[#F8F8FA]' : ''} rounded-3xl p-8 -ml-8 -mr-8`}>
+    <div className={`${text ? 'bg-[#F8F8FA]' : ''} rounded-3xl p-8 -ml-8 -mr-8`}>
       <Markdown>{text}</Markdown>
     </div>
   );
